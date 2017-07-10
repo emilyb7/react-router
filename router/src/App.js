1761344ca8913dc663444a76b1c4afec98b8b473
emily <limeyb7@gmail.com>
2017-07-10 19:15:14 +0100

    adds match.params logic

--- router/src/App.js
+++ router/src/App.js
@@ -2,12 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 /* router */
-import { BrowserRouter as Router, Route, Link, } from 'react-router-dom'
+import { BrowserRouter as Router, Route, } from 'react-router-dom'
+
+/* data */
+const sheepArray = [
+  {
+    name: 'Harry',
+    age: 4,
+  },
+  {
+    name: 'Nora',
+    age: 2,
+  },
+]
 
 /* components */
-const Home = props => ( <div>Home</div> )
+const Home = () => ( <div>Home</div> )
 
-const About = props => ( <div>About</div> )
+const About = () => ( <div>About</div> )
+
+const List = () => (
+  <div>
+    <ul>
+      { sheepArray.map( sheep => <li key={sheep.name}>{sheep.name}</li> ) }
+    </ul>
+  </div>
+)
+
+const Sheep = ({ match, }) => {
+  const matchingSheep = sheepArray.find((sheep) => sheep.name.toLowerCase() === match.params.name);
+  return (
+    <div>
+      <h1>{ matchingSheep.name }</h1>
+      <p>Age: { matchingSheep.age }</p>
+    </div>
+  )
+}
+
 
 class App extends Component {
   render() {
@@ -16,6 +47,8 @@ class App extends Component {
         <div>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
+          <Route path="/list" component={List} />
+          <Route path="/sheep/:name" component={Sheep}/>
         </div>
       </Router>
     );
