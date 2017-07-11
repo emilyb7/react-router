fdbfbc031c0588f45adb4daac4289b9a6bf89a82
emily <limeyb7@gmail.com>
2017-07-11 09:25:39 +0100

    add some links

--- router/src/App.js
+++ router/src/App.js
@@ -1,31 +1,40 @@
+/* learning React router with sheep */
+
 import React, { Component } from 'react';
 import './App.css';
 
 /* router */
-import { BrowserRouter as Router, Route, } from 'react-router-dom'
 
-/* data */
+import { BrowserRouter as Router, Route, Link, } from 'react-router-dom'
+
+/* some data */
+
 const sheepArray = [
-  {
-    name: 'Harry',
-    age: 4,
-  },
-  {
-    name: 'Nora',
-    age: 2,
-  },
+  { name: 'Harry', age: 4, },
+  { name: 'Nora', age: 2, },
 ]
 
 /* components */
-const Home = () => ( <div>Home</div> )
 
-const About = () => ( <div>About</div> )
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <Link to="/list">View all sheep</Link>
+  </div>
+)
+
+const About = () => ( <h1>About</h1> )
 
 const List = () => (
   <div>
+    <h1>Sheep</h1>
     <ul>
-      { sheepArray.map( sheep => <li key={sheep.name}>{sheep.name}</li> ) }
+      { sheepArray.map(sheep => <li key={sheep.name}>{sheep.name}</li>) }
     </ul>
+    <h2>Add a sheep</h2>
+    <form>
+
+    </form>
   </div>
 )
 
@@ -45,7 +54,10 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <p><Link to="/">Home</Link></p>
+          <p><Link to="/about">About</Link></p>
           <Route exact path="/" component={Home} />
+
           <Route path="/about" component={About} />
           <Route path="/list" component={List} />
           <Route path="/sheep/:name" component={Sheep}/>
